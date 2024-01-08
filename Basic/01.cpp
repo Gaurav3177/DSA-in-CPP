4d5344224590688ab0aeeaa5cc8838f270345271
Gaurav3177 <122596103+Gaurav3177@users.noreply.github.com>
2024-01-08 19:52:43 +0530

    Create 01.cpp

---
+++ Basic/01.cpp
@@ -0,0 +1,63 @@
+// Bitwise opereator
+/*
+And &
+or |
+not ~
+Xor ^
+These will work on bit level
+
+1.And(&)
+
+x   y    o
+0   0    0
+0   1    0 
+1   0    0
+1   1    1
+
+Ex  2&3
+   2= 10
+   3= 11
+      10=2
+
+Ex 5=101
+   7=111
+     101=5  
+
+2.Or(|)
+
+x   y    o
+0   0    0
+0   1    1 
+1   0    1
+1   1    1
+
+3.Not(~)
+
+x   y
+0   1
+1   0
+
+Ex
+a=2
+~a=?
+
+int = 4 bytes = 32 bits
+a=2         00000....010
+~a=         11111....101
+start 1=    -
+1's compli= -0000....010
+2's compli=           +1
+             0000....011
+             ans= -3
+
+4.Xor(^)
+
+x   y    o
+0   0    0
+0   1    1 
+1   0    1
+1   1    0
+
+
+
+*/
