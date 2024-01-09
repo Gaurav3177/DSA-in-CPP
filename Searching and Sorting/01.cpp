241df9d7a2a6aa7761e760f249e899e69e4cb43c
Gaurav3177 <122596103+Gaurav3177@users.noreply.github.com>
2024-01-09 23:07:59 +0530

    Update 01.cpp

--- Searching and Sorting/01.cpp
+++ Searching and Sorting/01.cpp
@@ -1,1 +1,32 @@
+// Linear Search
 
+#include<iostream>
+using namespace std;
+bool search(int arr[],int n,int key){
+    for(int i=0;i<n;i++){
+        if(key==arr[i]){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main(){
+    int n,key;
+    cout<<"Enter size of array = ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter key = ";
+    cin>>key;
+    bool found=search(arr,n,key);
+
+    if(found==1){
+        cout<<"Key found";
+    }
+    else{
+        cout<<"Key not found";
+    }
+
+}
